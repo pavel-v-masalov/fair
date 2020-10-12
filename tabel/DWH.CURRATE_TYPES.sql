29c299c77b3d4542afc5d0bf38b71334a9ef0a6d
Масалов Павел <pmasalov@masterdm.ru>
2020-10-12 12:06:47 +0300

    modify currate tytoe table

--- tabel/DWH.CURRATE_TYPES.sql
+++ tabel/DWH.CURRATE_TYPES.sql
@@ -1,11 +1,12 @@
 create table DWH.CURRATE_TYPES (
     CURRATE_TYPE_KEY NUMBER primary key,
-    CURRATE_TYPES_CD VARCHAR2(100),
-    CURRENCY_LETTER_CD VARCHAR2(100),
-    FIXFLOAT VARCHAR2(100)
+    CURRENCY_KEY NUMBER,
+    CURRENCY_LETTER_CD VARCHAR2(3),
+    FIXFLOAT VARCHAR2(5),
+    FIXFLOAT_CD VARCHAR2(100),
+    CONTRACTS_TERMS_KEY NUMBER
 );
 comment on table DWH.CURRATE_TYPES is 'Справочник типов ставки';
 comment on column DWH.CURRATE_TYPES.CURRATE_TYPE_KEY is 'Ключ типа ставки';
-comment on column DWH.CURRATE_TYPES.CURRATE_TYPES_CD is 'Название типа ставки';
 comment on column DWH.CURRATE_TYPES.CURRENCY_LETTER_CD is 'Название валюты';
 comment on column DWH.CURRATE_TYPES.FIXFLOAT is 'Признак фиксированной плавающей ставки';
